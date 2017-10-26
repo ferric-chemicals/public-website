ee0517f279110588088ec98a8c047e02eb0de9ec
Ferric Chemicals <ferric.chemicals@gmail.com>
2017-10-26 16:38:58 +0530

    Added Meta for mobile scalling

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ferric Chemicals Pvt. Ltd.</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <style type="text/css">
