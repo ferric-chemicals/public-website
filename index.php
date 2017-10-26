fa511bc69c4421e2f7c372da9fb9001d7d5274b0
Ferric Chemicals <ferric.chemicals@gmail.com>
2017-10-26 16:20:31 +0530

    Added Index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ferric Chemicals Pvt. Ltd.</title>
+</head>
+<body>
+
+    <center>
+        <h1>Website under development. Coming soon</h1>
+    </center>
+
+</body>
+</html>
