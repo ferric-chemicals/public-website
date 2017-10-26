a69bc845e6ba01b354f42c503d285f873dd11fb5
Ferric Chemicals <ferric.chemicals@gmail.com>
2017-10-26 16:25:36 +0530

    Added Logo and fonts

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 <html>
 <head>
     <title>Ferric Chemicals Pvt. Ltd.</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <style type="text/css">
+        * {
+            font-family: 'Open Sans', sans-serif;
+        }
+    </style>
 </head>
 <body>
 
     <center>
-        <h1>Website under development. Coming soon</h1>
+        <br/><br/>
+        <img src="logo.png" alt="Ferric Chemicals"/>
+        <br/>
+        <h1>Website under development. Coming soon</h1> 
+        <br/>
+            &copy; 2017 Ferric Chemicals. All Rights Reserved.
     </center>
 
 </body>
