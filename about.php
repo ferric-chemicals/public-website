446af46bceee6aaf0f93d640f09ce1c4c0086156
Bunny OOG Box <bunny.oogbox@gmail.com>
2017-12-09 14:20:58 +0530

    Added About Us page

--- about.php
+++ about.php
@@ -24,6 +24,43 @@ include_once("header.php");
         </div>
     </div>
 </section>
+
+<section id="blog-full-width">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <article class="wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">
+                    <div class="blog-post-image"></div>
+                    <div class="blog-content">
+                        <h2 class="blogpost-title">
+                            Go clean before everything.
+                        </h2>
+                        <p>We at <b>Ferric Chemicals</b> are a bunch of health freaks and out of box thinkers, visioning is to bring forth products that contributes better health and hygiene to all. We aspire to begin an evolution in health and hygiene by offering simple yet highly effective products for daily health care.
+                        </p>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="company-mission section-sm bg-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Our Mission</h3>
+                <p>To provide best quality Hand Sanitizer <br/>and make people's' life healthy.</p>
+                <img src="images/company-image-2.jpg" alt="" class="img-responsive mt-30">
+            </div>
+            <div class="col-md-6">
+                <h3>Our Vision</h3>
+                <p>To promote good Health and Hygiene thereby bringing smile across millions of faces</p>
+                <img src="images/company-image-3.jpg" alt="" class="img-responsive mt-30">
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php
 include_once("footer.php");
 ?>
--- index.php
+++ index.php
@@ -237,24 +237,6 @@ Slider Section Start
     </div>
 </section> <!-- #works -->
 
-<!--
-==================================================
-Call To Action Section Start
-================================================== -->
-<section id="call-to-action">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="block">
-                    <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">GET QUOTE FOR YOUR BUSINESS</h1>
-                    <p class="wow fadeInDown" data-wow-delay=".5s" data-wow-duration="500ms">We accept large orders for your business.</p>
-                    <a href="contact.php?product=b2b" class="btn btn-default  btn-contact wow fadeInDown">Contact us</a>
-                </div>
-            </div>
-            
-        </div>
-    </div>
-</section>
 <?php
 include_once("footer.php");
 ?>
