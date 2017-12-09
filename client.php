8f5226eef537f741250c194b14b90f50c36fc0fa
Bunny OOG Box <bunny.oogbox@gmail.com>
2017-12-09 15:08:12 +0530

    WIP Client page

---
+++ client.php
@@ -0,0 +1,36 @@
+<?php
+$pageTitle = "Our Clients";
+
+if(!isset($_GET["type"])) {
+    header("Location: clients.php");
+    die;
+}
+
+include_once("header.php");
+?>
+
+<!--  Page Content  -->
+<section class="global-page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="block">
+                    <h2>Our Clients</h2>
+                    <ol class="breadcrumb">
+                        <li>
+                            <a href="index.php">
+                                <i class="ion-ios-home"></i>
+                                Home
+                            </a>
+                        </li>
+                        <li class="active">Our Clients</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include_once("footer.php");
+?>
