1a0a945022f62187ecfc5ccc12ffe93856d5219e
Bunny OOG Box <bunny.oogbox@gmail.com>
2017-12-09 13:05:21 +0530

    Home Page ready

--- index.php
+++ index.php
@@ -3,9 +3,258 @@ $pageTitle = "Home";
 include_once("header.php");
 ?>
 
-<!--  Page Content  -->
+<!--
+==================================================
+Slider Section Start
+================================================== -->
+<section id="hero-area" >
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <div class="block wow fadeInUp" data-wow-delay=".3s">
+                
+                <!-- Slider -->
+                <section class="cd-intro">
+                    <h1 class="wow fadeInUp animated cd-headline slide" data-wow-delay=".4s" >
+                    <span>PROTECTION FROM GERMS</span><br>
+                    <span class="cd-words-wrapper">
+                        <b class="is-visible">GERM SLAYER HAND SANITIZER</b>
+                        <B>CLEAN HANDS & REMOVE GERMS</B>
+                        <b>KILLS 99.9% GERMS WITHOUT WATER</b>
+                        <B>COMES WITH DIFFERENT <br/>COLORS AND SIZES</B>
+                    </span>
+                    </h1>
+                    </section> <!-- cd-intro -->
+                    <!-- /.slider -->
+                    <h2 class="wow fadeInUp animated" data-wow-delay=".6s" >
+                        Get your hands clean instantly, anytime, anywhere. <br/>Just squeeze <b>Germ Slayer</b> squirt and experience the freshness
+                    </h2>
+                    <a class="btn btn-default btn-green" href="/products.php">View Products</a>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!--/#main-slider-->
+<section id="works" class="works">
+    <div class="container">
+        <div class="section-heading">
+            <br/>
+            <center><img class="wow fadeInDown" src="/images/logo_small.png" alt="Ferric Chemicals"/>
+            <h1 class="title wow fadeInDown" data-wow-delay=".3s"><b>Welcome to Ferric Chemicals Pvt. Ltd.</h1>
+            <center><img class="wow fadeInDown" src="/images/germ_slayer_small.png" alt="Germ Slayer by Ferric Chemicals"/></center>
+            <br/>
+            <p class="wow fadeInDown" data-wow-delay=".5s">
+                We are manufacturer of <b>Hand Sanitizer</b> in Ahmedabad, Gujarat. Our main brand <b>Germ Slayer Hand Sanitizer</b> is an Instant hand sanitizer which kills 99.9% germs from hands, it protects you and your loved ones against bacteria, viruses and fungi. Germ Slayer is your partner for good health and hygiene.
+            </p>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 col-xs-12">
+                <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="0ms">
+                    <div class="img-wrapper">
+                        <img src="images/portfolio/item-1.jpg" class="img-responsive" alt="Sachet (1.5 ML)" >
+                        <div class="overlay">
+                            <div class="buttons">
+                                <a href="contact.php?product=sachet_1_5_ml">Get Quote</a>
+                                <a href="sachet_1_5_ml.php">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <figcaption>
+                    <h4>
+                    <a href="sachet_1_5_ml.php">
+                        Sachet (1.5ML)
+                    </a>
+                    </h4>
+                    <p>
+                        Available in three colors (White, Blue and Pink)
+                    </p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
+                    <div class="img-wrapper">
+                        <img src="images/portfolio/item-2.jpg" class="img-responsive" alt="Band Sanitizer (30 ML)" >
+                        <div class="overlay">
+                            <div class="buttons">
+                                <a href="/contact.php?product=band_sanitizer_30_ml">Get Quote</a>
+                                <a href="band_sanitizer_30_ml.php">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <figcaption>
+                    <h4>
+                    <a href="band_sanitizer_30_ml.php">
+                        Band Sanitizer (30 ML)
+                    </a>
+                    </h4>
+                    <p>
+                        Available in three colors (White, Blue and Pink)
+                    </p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
+                    <div class="img-wrapper">
+                        <img src="images/portfolio/item-3.jpg" class="img-responsive" alt="Sanitizer (50ML) Bottle" >
+                        <div class="overlay">
+                            <div class="buttons">
+                                <a href="/contact.php?product=sanitizer_50_ml_bottle">Get Quote</a>
+                                <a href="sanitizer_50_ml_bottle.php">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <figcaption>
+                    <h4>
+                    <a href="sanitizer_50_ml_bottle.php">
+                        Sanitizer (50ML) in Bottle
+                    </a>
+                    </h4>
+                    <p>
+                        Available in two colors (White and Pink)
+                    </p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="600ms">
+                    <div class="img-wrapper">
+                        <img src="images/portfolio/item-4.jpg" class="img-responsive" alt="" >
+                        <div class="overlay">
+                            <div class="buttons">
+                                <a href="contact.php?product=sanitizer_100_ml_family_pack">Get Quote</a>
+                                <a href="sanitizer_100_ml_family_pack.php">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <figcaption>
+                    <h4>
+                    <a href="sanitizer_100_ml_family_pack.php">
+                        Sanitizer (100 ML) Family Pack
+                    </a>
+                    </h4>
+                    <p>
+                        Available in three colors (White, Blue and Pink)
+                    </p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="900ms">
+                    <div class="img-wrapper">
+                        <img src="images/portfolio/item-5.jpg" class="img-responsive" alt="" >
+                        <div class="overlay">
+                            <div class="buttons">
+                                <a href="contact.php?product=sanitizer_250_ml_family_pack">Get Quote</a>
+                                <a href="sanitizer_250_ml_family_pack.php">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <figcaption>
+                    <h4>
+                    <a href="sanitizer_250_ml_family_pack.php">
+                        Sanitizer (250 ML) Family Pack
+                    </a>
+                    </h4>
+                    <p>
+                        Available in three colors (White, Blue and Pink)
+                    </p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="1200ms">
+                    <div class="img-wrapper">
+                        <img src="images/portfolio/item-6.jpg" class="img-responsive" alt="" >
+                        <div class="overlay">
+                            <div class="buttons">
+                                <a href="contact.php?product=sanitizer_500_mml_jumbo">Get Quote</a>
+                                <a href="sanitizer_500_mml_jumbo.php">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <figcaption>
+                    <h4>
+                    <a href="sanitizer_500_mml_jumbo.php">
+                        Sanitizer (500 ML) Jumbo Pack
+                    </a>
+                    </h4>
+                    <p>
+                        Available in three colors (White, Blue and Pink)
+                    </p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="600ms">
+                    <div class="img-wrapper">
+                        <img src="images/portfolio/item-7.jpg" class="img-responsive" alt="" >
+                        <div class="overlay">
+                            <div class="buttons">
+                                <a href="contact.php?product=sanitizer_1_ltr">Get Quote</a>
+                                <a href="sanitizer_1_ltr.php">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <figcaption>
+                    <h4>
+                    <a href="sanitizer_1_ltr.php">
+                        Sanitizer (1 Liter) Large Pack
+                    </a>
+                    </h4>
+                    <p>
+                        Available in three colors (White, Blue and Pink)
+                    </p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="900ms">
+                    <div class="img-wrapper">
+                        <img src="images/portfolio/item-8.jpg" class="img-responsive" alt="" >
+                        <div class="overlay">
+                            <div class="buttons">
+                                <a href="contact.php?product=sanitizer_5_ltr">Get Quote</a>
+                                <a href="sanitizer_5_ltr.php">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <figcaption>
+                    <h4>
+                    <a href="sanitizer_5_ltr.php">
+                        Sanitizer (5 Liter) Large Pack
+                    </a>
+                    </h4>
+                    <p>
+                        Available in three colors (White, Blue and Pink)
+                    </p>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
+</section> <!-- #works -->
 
-
+<!--
+==================================================
+Call To Action Section Start
+================================================== -->
+<section id="call-to-action">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="block">
+                    <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">GET QUOTE FOR YOUR BUSINESS</h1>
+                    <p class="wow fadeInDown" data-wow-delay=".5s" data-wow-duration="500ms">We accept large orders for your business.</p>
+                    <a href="contact.php?product=b2b" class="btn btn-default  btn-contact wow fadeInDown">Contact us</a>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
 <?php
 include_once("footer.php");
 ?>
